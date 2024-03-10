9c53eba9e6a881bc834c8b87e8a40654602fbc4f
StephenWez <a18588430291@163.com>
2024-03-10 17:51:39 +0800

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -6,29 +6,30 @@ using std::vector;
 std::vector<std::pair<int, Action>>  SimpleTransportStrategy::scheduleRobots(std::vector<Robot> &robots, const Map &map, std::vector<Goods> &goods, std::vector<Berth> &berths)
 {
     // 寻路
-    vector<vector<vector<Point2d>>> path2goods(robots.size(), vector<vector<Point2d>>(goods.size(), vector<Point2d>())), \
-                                    path2berths(robots.size(), vector<vector<Point2d>>(berths.size(), vector<Point2d>())); //机器人到达货物\泊位的路径
+    AStarPathfinder pathfinder;
+    vector<vector<std::variant<Path, PathfindingFailureReason>>> path2goods(robots.size(), vector<std::variant<Path, PathfindingFailureReason>>(goods.size(), std::variant<Path, PathfindingFailureReason>())), \
+                                    path2berths(robots.size(), vector<std::variant<Path, PathfindingFailureReason>>(berths.size(), std::variant<Path, PathfindingFailureReason>())); //机器人到达货物\泊位的路径
     for (int i=0;i<robots.size();i++) {
         for (int j=0;j<goods.size();j++) {
-            path2goods[i][j] = AStarPathfinder::findPath(robots[i].pos, goods[j].pos, map);
+            path2goods[i][j] = pathfinder.findPath(robots[i].pos, goods[j].pos, map);
         }
         for (int j=0;j<berths.size();j++) {
-            path2berths[i][j] = AStarPathfinder::findPath(robots[i].pos, berths[j].pos, map);
+            path2berths[i][j] = pathfinder.findPath(robots[i].pos, berths[j].pos, map);
         }
     }
-    vector<vector<vector<Point2d>>> path2berth(goods.size(), vector<vector<Point2d>>(berths.size(), vector<Point2d>())); //货物到泊位的路径
+    vector<vector<std::variant<Path, PathfindingFailureReason>>> path2berth(goods.size(), vector<std::variant<Path, PathfindingFailureReason>>(berths.size(), std::variant<Path, PathfindingFailureReason>())); //货物到泊位的路径
     for (int i=0;i<goods.size();i++) {
         for (int j=0;j<berths.size();j++) {
-            path2berth[i][j] = AStarPathfinder::findPath(goods[i].pos, berths[j].pos, map);
+            path2berth[i][j] = pathfinder.findPath(goods[i].pos, berths[j].pos, map);
         }
     }
     
     // 衡量收益：机器人（未携带货物）<->货物<->泊位 | 机器人（携带货物）<->泊位
     // 收益 = 货物利润 / 机器人将货物送达泊位的耗时
-    vector<int> robots_gain(robots.size(), int());
+    vector<float> robots_gain(robots.size());
     for (int i=0;i<robots.size();i++) {
         if (robots[i].carryingItem) {
-            // robots_gain[i] = 
+            robots_gain[i] = float(goods[robots[i].carryingItemId].value) / 
         }
     }
     

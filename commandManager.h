1128b6ca1a068aa4d77cb0411de1f63dfd3d7437
skyhigh404 <ywq12353004412@126.com>
2024-03-11 10:38:40 +0800

    modify commandManager

--- commandManager.h
+++ commandManager.h
@@ -6,6 +6,7 @@
 #include "robot.h"
 #include "ship.h"
 #include "goods.h"
+#include <sstream>
 // 这个类负责收集机器人和船只的指令，并最终统一输出。
 class CommandManager
 {
@@ -24,14 +25,16 @@ public:
     }
     void outputCommands() const
     {
+        std::ostringstream stream;
         // 输出所有机器人指令
         for (const auto &command : robotCommands)
-            std::cout << command << std::endl;
+            stream << command << "\n";
         // 输出所有船舶指令
         for (const auto &command : shipCommands)
-            std::cout << command << std::endl;
+            stream << command << "\n";
+        stream << "OK\n";
         // 刷新输出缓冲区
-        std::cout << std::flush;
+        std::cout << stream.str() << std::flush;
     }
     void clearCommands()
     {

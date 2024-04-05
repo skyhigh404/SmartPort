2a0567ef742141b0ffeeec3596addb3f23bd37e2
StephenWez <a18588430291@163.com>
2024-04-05 19:05:58 +0800

    购买策略更新

--- earlyGameAssetManager.cpp
+++ earlyGameAssetManager.cpp
@@ -22,8 +22,12 @@ void EarlyGameAssetManager::init(const Map& map, const std::vector<Berth> &berth
         }
     }
 
-    divideLandConnectedBlocks(berths, gameMap);
-    divideSeaConnectedBlocks();
+    divideLandAndSeaConnectedBlocks(berths, gameMap);
+    purchasedRobotNum = std::vector<int>(landseaBlocks.size(), 0);
+    purchasedShipNum = std::vector<int>(landseaBlocks.size(), 0);
+    // robotPurchaseAssign = std::vector<int>(landseaBlocks.size(), 0);
+    // shipPurchaseAssign = std::vector<int>(landseaBlocks.size(), 0);
+    // divideSeaConnectedBlocks(berths, gameMap.deliveryLocations, gameMap);
 }
 
 std::vector<PurchaseDecision> EarlyGameAssetManager::makePurchaseDecision(const Map &gameMap,
@@ -78,6 +82,109 @@ void EarlyGameAssetManager::divideLandConnectedBlocks(const std::vector<Berth> &
     }
 }
 
+void EarlyGameAssetManager::divideSeaConnectedBlocks(const std::vector<Berth> &berths, const std::vector<Point2d> &deliveryLocations, const Map &map)
+{
+    // 划分海洋连通块
+    std::vector<bool> clustered(deliveryLocations.size(), false);
+    for (int i = 0; i < deliveryLocations.size(); i++)
+    {
+        const Point2d& delivery1 = deliveryLocations[i];
+        if (!clustered[i])
+        {
+            std::vector<Point2d> anotherClass;
+            anotherClass.push_back(deliveryLocations[i]);
+            for (int j = i + 1; j < deliveryLocations.size(); j++)
+            {
+                const Point2d& delivery2 = deliveryLocations[j];
+                for (int k = 0; k < berths.size(); k++)
+                    if (map.maritimeBerthDistanceMap.at(k)[delivery1.x][delivery1.y] != INT_MAX && map.maritimeBerthDistanceMap.at(k)[delivery2.x][delivery2.y] != INT_MAX)
+                    {
+                        anotherClass.push_back(deliveryLocations[j]);
+                        clustered[j] = true;
+                    }
+            }
+            seaBlocks.push_back(SeaBlock{0, anotherClass});
+        }
+    }
+
+    // 确定连通块大小
+    // for (SeaBlock& sb: seaBlocks) {
+    //     int count_passableBlock = 0;
+    //     for (int i=0;i<map.rows;i++) {
+    //         for (int j=0;j<map.cols;j++) {
+    //             if (map.maritimeBerthDistanceMap.at(sb.berths[0].id)[i][j] < INT_MAX) 
+    //                 count_passableBlock++;
+    //         }
+    //     }
+    //     sb.size = count_passableBlock;
+    // }
+}
+
+void EarlyGameAssetManager::divideLandAndSeaConnectedBlocks(const std::vector<Berth> &berths, const Map &map)
+{
+    const std::vector<Point2d> &deliveryLocations = map.deliveryLocations;
+    std::vector<bool> clustered(deliveryLocations.size(), false);
+    // 对交货点进行连通性聚类
+    for (int i = 0; i < deliveryLocations.size(); i++)
+    {
+        const Point2d& delivery1 = deliveryLocations[i];
+        if (!clustered[i])
+        {
+            std::vector<Point2d> anotherDeliveryLocations;
+            anotherDeliveryLocations.push_back(deliveryLocations[i]);
+            for (int j = i + 1; j < deliveryLocations.size(); j++)
+            {
+                const Point2d& delivery2 = deliveryLocations[j];
+                for (int k = 0; k < berths.size(); k++)
+                    if (map.maritimeBerthDistanceMap.at(k)[delivery1.x][delivery1.y] != INT_MAX && map.maritimeBerthDistanceMap.at(k)[delivery2.x][delivery2.y] != INT_MAX)
+                    {
+                        anotherDeliveryLocations.push_back(deliveryLocations[j]);
+                        clustered[j] = true;
+                    }
+            }
+            landseaBlocks.push_back(LandSeaBlock{0, {}, anotherDeliveryLocations, {}, {}});
+        }
+    }
+
+    for (int i=0;i<landseaBlocks.size();i++) {
+        LandSeaBlock& lsb = landseaBlocks[i];
+        // 找出相连通的泊位
+        std::vector<Berth> connectedBerths;
+        for (int j=0;j<berths.size();j++) {
+            const Berth& berth = berths[j];
+            if (map.maritimeBerthDistanceMap.at(berth.id)[lsb.deliveryLocations[0].x][lsb.deliveryLocations[0].y] != INT_MAX) {
+                connectedBerths.push_back(berth);
+            }
+        }
+        // 找出可用的机器人购买点
+        std::vector<Point2d> availableRobotShops;
+        for (int j=0;j<robotShops.size();j++) {
+            Point2d& rs = robotShops[j];
+            for (int k=0;k<connectedBerths.size();k++) {
+                const Berth& berth = connectedBerths[k];
+                if (map.berthDistanceMap.at(berth.id)[rs.x][rs.y] != INT_MAX) {
+                    availableRobotShops.push_back(robotShops[j]);
+                    break;
+                }
+            }
+        }
+        // 找出可用的轮船购买点
+        std::vector<Point2d> availableShipShops;
+        for (int j=0;j<shipShops.size();j++) {
+            Point2d& ss = shipShops[j];
+            for (int k=0;k<connectedBerths.size();k++) {
+                const Berth& berth = connectedBerths[k];
+                if (map.maritimeBerthDistanceMap.at(berth.id)[ss.x][ss.y] != INT_MAX) {
+                    availableShipShops.push_back(shipShops[j]);
+                    break;
+                }
+            }
+        }
+        landseaBlocks[i].berths = connectedBerths;
+        landseaBlocks[i].robotShops = availableRobotShops;
+        landseaBlocks[i].shipShops = availableShipShops;
+    }
+}
 bool EarlyGameAssetManager::needToBuyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds)
 {
     // 超出最大限制
@@ -93,21 +200,45 @@ bool EarlyGameAssetManager::needToBuyShip(const std::vector<Ship> &ships, const 
     return true;
 }
 
-void EarlyGameAssetManager::buyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds)
+Point2d EarlyGameAssetManager::buyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds)
 {
-    // 评估购买点:根据连通块大小排序然后优先选择最大的购买
-    std::vector<int> shopValue(robotShops.size(), 0);
-    for (int i=0;i<robotShops.size();i++) {
-
+    for (int phase=0; phase<robotPurchaseAssign.size(); phase++) {
+        // 按阶段进行购买
+        for (int i=0; i<landseaBlocks.size(); i++) {
+            // 当前联通块已完成本阶段购买
+            if (purchasedRobotNum[i] >= robotPurchaseAssign[i][phase]) continue;
+            // 为当前联通块购买机器人：找合适的购买点
+            purchasedRobotNum[i]++; // 暂时在这更新，可能要移动到processFramedata去
+            return getProperRobotShop(landseaBlocks[i], gameMap);
+        }
     }
-    // 购买
-}
-bool EarlyGameAssetManager::buyShip(const std::vector<Ship> &ships, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds)
-{
-    return false;
+    // 不购买 或 购买失败
+    return Point2d(-1,-1);
 }
 
-void EarlyGameAssetManager::divideSeaConnectedBlocks()
+Point2d EarlyGameAssetManager::buyShip(const std::vector<Ship> &ships, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds)
 {
-    LOGE("divideSeaConnectedBlocks 未实现");
+    for (int phase=0; phase<shipPurchaseAssign.size(); phase++) {
+        // 按阶段进行购买
+        for (int i=0; i<landseaBlocks.size(); i++) {
+            // 当前联通块已完成本阶段购买
+            if (purchasedShipNum[i] >= shipPurchaseAssign[i][phase]) continue;
+            // 为当前联通块购买机器人：找合适的购买点
+            purchasedShipNum[i]++; // 暂时在这更新，可能要移动到processFramedata去
+            return getProperShipShop(landseaBlocks[i], gameMap);
+        }
+    }
+    // 不购买 或 购买失败
+    return Point2d(-1,-1);
+}
+
+Point2d EarlyGameAssetManager::getProperRobotShop(LandSeaBlock& block, const Map &gameMap)
+{
+    //未完成
+return Point2d(-1,-1);
+}
+Point2d EarlyGameAssetManager::getProperShipShop(LandSeaBlock& block, const Map &gameMap)
+{
+    //未完成
+return Point2d(-1,-1);
 }
--- earlyGameAssetManager.h
+++ earlyGameAssetManager.h
@@ -13,7 +13,15 @@ struct LandBlock
 struct SeaBlock
 {
     int size; // 面积
-    // std::vector<Berth> berths; // 交货点
+    std::vector<Point2d> deliveryLocations; // 交货点
+};
+struct LandSeaBlock
+{
+    int landSize;
+    std::vector<Berth> berths;
+    std::vector<Point2d> deliveryLocations;
+    std::vector<Point2d> robotShops;
+    std::vector<Point2d> shipShops;
 };
 
 // 追求长期利润
@@ -38,7 +46,10 @@ public:
     std::vector<Point2d> shipShops;
     std::vector<LandBlock> landBlocks;
     std::vector<SeaBlock> seaBlocks;
+    std::vector<LandSeaBlock> landseaBlocks;
     
+    std::vector<int> purchasedRobotNum;
+    std::vector<int> purchasedShipNum;
 
 public:
     void init(const Map& map, const std::vector<Berth> &berths, const Map &gameMap); //初始化商店,可以在gamemanager中初始化
@@ -47,12 +58,18 @@ private:
     // 超参数
     int maxRobotNum;    // 最多购买机器人数目
     int maxShipNum;     // 最多购买船只数目
+    std::vector<std::vector<int>> robotPurchaseAssign;
+    std::vector<std::vector<int>> shipPurchaseAssign;
+    int startNum;       // 最初的数目（机器人、轮船）
 
 private:
     void divideLandConnectedBlocks(const std::vector<Berth> &berths, const Map &map);
-    void divideSeaConnectedBlocks(); //TODO：根据交货点划分
+    void divideSeaConnectedBlocks(const std::vector<Berth> &berths, const std::vector<Point2d> &deliveryLocations, const Map &map);
+    void divideLandAndSeaConnectedBlocks(const std::vector<Berth> &berths, const Map &map);
     bool needToBuyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
     bool needToBuyShip(const std::vector<Ship> &ships, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
-    void buyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
-    bool buyShip(const std::vector<Ship> &ships, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
+    Point2d buyRobot(const std::vector<Robot> &robots, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
+    Point2d buyShip(const std::vector<Ship> &ships, const std::vector<Goods> &goods, const Map &gameMap, int currentFunds);
+    Point2d getProperRobotShop(LandSeaBlock& l, const Map &gameMap);
+    Point2d getProperShipShop(LandSeaBlock& l, const Map &gameMap);
 };
